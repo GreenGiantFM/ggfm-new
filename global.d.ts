33cc86cc2373c81f1d3e45200fdd6551d6c5002e
Jared Sy <jaredblase@gmail.com>
2022-07-29 02:40:27 +0800

    add: metadata

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			NEXT_PUBLIC_SITE_TITLE: string
+		}
+	}
+}
---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const canonical = 'https://greengiantfm.com/'
+const description = "DLSU Radio: Green Giant FM (GGFM) is De La Salle University-Manila&apos;s Official Radio Station, located and broadcasting live at Br. Bloemen Hall!"
+const image = '/images/card-image.png'
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head>
+				<meta name="description" content={description} />
+
+				<meta property="og:url" content={canonical} />
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={process.env.NEXT_PUBLIC_SITE_TITLE} />
+				<meta property="og:description" content={description} />
+				<meta property="og:image" content={image} />
+				<meta property="fb:app_id" content="585506388489406" />
+
+				<link rel="canonical" href={canonical} />
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:title" content={process.env.NEXT_PUBLIC_SITE_TITLE} />
+				<meta property="twitter:description" content={description} />
+				<meta property="twitter:site" content="@GreenGiantFM" />
+				<meta property="twitter:creator" content="@GreenGiantFM" />
+				<meta property="twitter:image" content={image} />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
