71c32424c548013873a052230cbb7140c18a808c
Jared Sy <jaredblase@gmail.com>
2022-09-07 16:18:43 +0800

    add: font awesome dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import Layout from '@components/layouts/main'
 import { ToastContainer } from 'react-toastify'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
 
 function MyApp({
 	Component,
