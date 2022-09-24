6b4f65383feb37534e629ebbc6990a2ccd7ce2dc
Jared Sy <jaredblase@gmail.com>
2022-09-24 21:00:33 +0800

    add(social media banner): aria label

--- components/social-media-banner.tsx
+++ components/social-media-banner.tsx
@@ -11,17 +11,17 @@ import {
 
 export default function SocialMediaBanner() {
 	return (
-		<section className="grid place-items-center bg-white text-neutral-900 py-2 sm:py-0">
+		<section className="grid place-items-center bg-white text-neutral-900 !py-6">
 			<div>
 				<h1 className="text-4xl sm:text-6xl text-center">Keep it locked with us!</h1>
 				<div
 					className="text-primary flex justify-center space-x-6 sm:space-x-10 mt-4 sm:mt-6 text-3xl sm:text-5xl">
-					<a href={URLs.twitter}><FontAwesomeIcon icon={faTwitter} /></a>
-					<a href={URLs.facebook}><FontAwesomeIcon icon={faFacebookF} /></a>
-					<a href={URLs.youtube}><FontAwesomeIcon icon={faYoutube} /></a>
-					<a href={URLs.spotify}><FontAwesomeIcon icon={faSpotify} /></a>
-					<a href={URLs.instagram}><FontAwesomeIcon icon={faInstagram} /></a>
-					<a href={URLs.linkedin}><FontAwesomeIcon icon={faLinkedinIn} /></a>
+					<a href={URLs.twitter} aria-label="Twitter profile link"><FontAwesomeIcon icon={faTwitter} /></a>
+					<a href={URLs.facebook} aria-label="Facebook profile link"><FontAwesomeIcon icon={faFacebookF} /></a>
+					<a href={URLs.youtube} aria-label="Youtube channel link"><FontAwesomeIcon icon={faYoutube} /></a>
+					<a href={URLs.spotify} aria-label="Spotify account link"><FontAwesomeIcon icon={faSpotify} /></a>
+					<a href={URLs.instagram} aria-label="Instagram profile link"><FontAwesomeIcon icon={faInstagram} /></a>
+					<a href={URLs.linkedin} aria-label="LinkedIn profile link"><FontAwesomeIcon icon={faLinkedinIn} /></a>
 				</div>
 			</div>
 		</section>
