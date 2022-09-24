63e6dcc386a32c5e516b13415ea9ea4e98b4f12f
Jared Sy <jaredblase@gmail.com>
2022-09-24 15:47:35 +0800

    add: next font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-const canonical = 'https://greengiantfm.com/'
 const description = "DLSU Radio: Green Giant FM (GGFM) is De La Salle University-Manila&apos;s Official Radio Station, located and broadcasting live at Br. Bloemen Hall!"
 const image = '/images/card-image.png'
 
@@ -21,7 +20,7 @@ export default function Document() {
 
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-				<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,600;1,600&display=swap" rel="stylesheet" />
+				<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,600;1,600&display=swap" rel="stylesheet" />
 			</Head>
 			<body>
 				<Main />
--- windi.config.ts
+++ windi.config.ts
@@ -7,17 +7,22 @@ export default defineConfig({
 			colors: {
 				primary: '#569429',
 				secondary: '#D8CEAE',
+				'primary-dark': '#2E5115',
 				neutral: {
 					...colors.neutral as Record<number, string>,
 					600: '#646464',
 					700: '#363636',
 					800: '#262626',
 					900: '#191919'
-				}
+				},
 			},
 			fontFamily: {
 				primary: ['Kenyan Coffee', 'Oswald SemiBold', 'Verdana', 'sans-serif'],
 				secondary: ['Raleway', 'Roboto', 'Verdana', 'sans-serif'],
+				next: ['The Next Font', 'Open Sans', 'Verdana', 'sans-serif']
+			},
+			boxShadow: {
+				progress: 'inset 0 -4px 8px 0 rgba(0, 0, 0, 0.6)',
 			}
 		}
 	},
