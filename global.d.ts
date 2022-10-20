47b1cc65cc842fa85a9c55c090c243493ede49c2
Jared Sy <jaredblase@gmail.com>
2022-10-21 00:27:40 +0800

    add: utility function

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,19 @@
+import mongoose from 'mongoose'
+
+export type MongooseConnection = {
+	conn: typeof mongoose | null;
+	promise: Promise<typeof mongoose> | null;
+}
+
 declare global {
+	var mongoose: MongooseConnection
+
 	namespace NodeJS {
 		interface ProcessEnv {
 			NEXT_PUBLIC_SITE_TITLE: string
 			NEXT_PUBLIC_VERCEL_URL: string
 			NEXT_PUBLIC_API_BASE_URL: string
+			MONGODB_URI: string
 		}
 	}
 }
