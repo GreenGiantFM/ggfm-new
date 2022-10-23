c6e0ee7c51cf8b1f180a309dcaf55fa4b683d2c5
Jared Sy <jaredblase@gmail.com>
2022-10-24 00:03:57 +0800

    add: rt slider component

---
+++ components/swipers/radio-talents.tsx
@@ -0,0 +1,64 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { A11y, Autoplay, Navigation } from 'swiper'
+import Image from 'next/future/image'
+import { LeftButton, RightButton } from './nav-buttons'
+import { useRef } from 'react'
+import 'swiper/css'
+
+type RadioTalentsProps = {
+	images: {
+		src: string
+		alt: string
+	}[],
+	className?: string
+	setIndex: (index: number) => void
+}
+
+export default function RadioTalents({ images, className, setIndex }: RadioTalentsProps) {
+	const navPrevRef = useRef<HTMLButtonElement>(null)
+	const navNextRef = useRef<HTMLButtonElement>(null)
+
+	const onBeforeInit = (Swiper: SwiperCore): void => {
+		const navigation = Swiper.params.navigation;
+
+		if (navigation && typeof navigation !== 'boolean') {
+			navigation.prevEl = navPrevRef.current
+			navigation.nextEl = navNextRef.current
+		}
+	}
+
+	return (
+		<Swiper
+			modules={[A11y, Autoplay, Navigation]}
+			grabCursor
+			lazy
+			slidesPerView='auto'
+			watchSlidesProgress
+			onBeforeInit={onBeforeInit}
+			loop
+			loopedSlides={images.length}
+			centeredSlides
+			autoplay={{
+				delay: 5000,
+				disableOnInteraction: false
+			}}
+			className={className + ' radio-talents'}
+			onSlideChange={(swiper) => setIndex(swiper.realIndex)}
+		>
+			{images.map(({ src, alt }) => (
+				<SwiperSlide key={alt} className="flex justify-center !h-auto !w-64 transform transition-transform">
+					<div className="h-98 w-auto">
+						<Image src={`https://drive.google.com/uc?export=view&id=${src}`}
+							className="w-full h-full object-contain opacity-70"
+							alt={alt}
+							width={270}
+							height={480}
+						/>
+					</div>
+				</SwiperSlide>
+			))}
+			<LeftButton className="w-8" ref={navPrevRef} />
+			<RightButton className="w-8" ref={navNextRef} />
+		</Swiper>
+	)
+}
