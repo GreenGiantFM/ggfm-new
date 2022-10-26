3fccec5e51874a1ebb8f66ba43da85b9526a3e3c
Jared Sy <jaredblase@gmail.com>
2022-10-27 03:18:29 +0800

    add: axios error util

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const dateOptions: Intl.DateTimeFormatOptions = { weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' }
 
 /**
@@ -5,10 +7,22 @@ const dateOptions: Intl.DateTimeFormatOptions = { weekday: 'long', month: 'short
  * @param date - a date object or date string to format
  * @returns a string equivalent of the date in the form 'MMM-DD-YYYY'
  */
- export function formatDate(date: Date | string) {
+export function formatDate(date: Date | string) {
 	if (typeof date == 'string') {
 		date = new Date(date)
 	}
 
 	return date.toLocaleDateString('en-US', dateOptions)
+}
+
+/**
+ * Utility function for retrieving an axios error message in TypeScript
+ * @param error - an error object of unknown type
+ */
+export function getAxiosError(error: unknown) {
+	if (axios.isAxiosError(error)) {
+		return error.response?.data as string ?? error.message
+	}
+
+	return 'An error has occured!'
 }
