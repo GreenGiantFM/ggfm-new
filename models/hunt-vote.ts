6b0d36cf618a3af4336a887fd4dc7707ecaec4e8
Jared Sy <jaredblase@gmail.com>
2022-10-27 03:18:21 +0800

    add: hunt vote model

---
+++ models/hunt-vote.ts
@@ -0,0 +1,13 @@
+import { Model, models, model, Schema } from 'mongoose'
+
+export interface IHuntVote {
+	email: string
+	candidate: Schema.Types.ObjectId | string
+}
+
+const schema = new Schema<IHuntVote>({
+	email: { type: String, required: true, trim: true },
+	candidate: { type: Schema.Types.ObjectId, ref: 'DJTrainee' },
+}, { versionKey: false })
+
+export default models?.HuntVote as Model<IHuntVote & Document> || model<IHuntVote>('HuntVote', schema, 'huntvotes')
