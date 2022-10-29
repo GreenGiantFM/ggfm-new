7fef0866b3162930fb75893b192e165ca4a6a8e8
Jared Sy <jaredblase@gmail.com>
2022-10-30 01:58:20 +0800

    add: date model

---
+++ models/dates.ts
@@ -0,0 +1,15 @@
+import { Model, models, model, Schema } from 'mongoose'
+
+export interface IDate {
+	name: string
+	start: Date
+	end: Date
+}
+
+const schema = new Schema<IDate>({
+	name: { type: String, required: true, trim: true },
+	start: { type: Date, required: true },
+	end: { type: Date, required: true }
+}, { versionKey: false })
+
+export default models?.Dates as Model<IDate & Document> || model<IDate>('Dates', schema, 'dates')
