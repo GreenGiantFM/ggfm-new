b38ed56a1028d30a5009b9ca3755bf2d78652896
Jared Sy <jaredblase@gmail.com>
2022-10-29 23:36:54 +0800

    add: data types

--- global.d.ts
+++ global.d.ts
@@ -8,12 +8,17 @@ export type MongooseConnection = {
 declare global {
 	var mongoose: MongooseConnection
 
+	interface Window {
+		google: any;
+	}
+
 	namespace NodeJS {
 		interface ProcessEnv {
 			NEXT_PUBLIC_SITE_TITLE: string
 			NEXT_PUBLIC_VERCEL_URL: string
 			NEXT_PUBLIC_API_BASE_URL: string
 			MONGODB_URI: string
+			NEXT_PUBLIC_GOOGLE_CLIENT_ID: string
 		}
 	}
 }
