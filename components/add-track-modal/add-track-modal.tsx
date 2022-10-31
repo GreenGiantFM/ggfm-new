dfa8f9aefb8135aa3848a84192cbce7f67e11479
Jared Sy <jaredblase@gmail.com>
2022-11-01 01:21:22 +0800

    add: add-track-modal component

---
+++ components/add-track-modal/add-track-modal.tsx
@@ -0,0 +1,51 @@
+import { Modal } from '@components/modal'
+import { IModalProps } from '@components/modal/modal'
+import { Dialog } from '@headlessui/react'
+import { app } from '@lib/axios-config'
+import { getAxiosError } from '@lib/utils'
+import styles from '@styles/Modal.module.css'
+import { FormEventHandler, useState } from 'react'
+
+type AddTrackModalProps = Omit<IModalProps, 'close'> & {
+	close: () => void
+}
+
+export function AddTrackModal({ ...props }: AddTrackModalProps) {
+	const [error, setError] = useState('')
+
+	const handleSubmit: FormEventHandler<HTMLFormElement> = async e => {
+		e.preventDefault()
+		const { tracks } = Object.fromEntries(new FormData(e.target as HTMLFormElement)) as { tracks: string }
+
+		if (!tracks) return setError('Cannot be empty!')
+
+		try {
+			await app.post('/api/hitlists/tracks', {
+				tracks: tracks
+			})
+			props.close()
+		} catch (e) {
+			console.error(getAxiosError(e))
+		}
+	}
+
+	return (
+		<Modal {...props}>
+			<div className={styles.panel}>
+				<div className={styles.body}>
+					<div>
+						<Dialog.Title as="h1" className={styles.title}>
+							Add tracks
+						</Dialog.Title>
+					</div>
+					<form onSubmit={handleSubmit}>
+						<label htmlFor="track-input" className="select-none">Tracks (ID&apos;s only and comma-separated)</label>
+						<textarea name="tracks" id="track-input" className="w-full p-1 border-2 rounded font-sans" rows={10} onChange={() => setError('')}></textarea>
+						<p className="min-h-5 mb-4 text-sm text-red-600">{error}</p>
+						<input type="submit" value="Submit" className="btn green py-2 rounded" />
+					</form>
+				</div>
+			</div>
+		</Modal>
+	)
+}
---
+++ components/add-track-modal/index.ts
@@ -0,0 +1,1 @@
+export { AddTrackModal } from './add-track-modal'
