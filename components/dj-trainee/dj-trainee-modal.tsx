fbe91f6b974a62d90940060a695a4097ea4f7423
Jared <jaredblase@gmail.com>
2022-11-22 15:50:47 +0800

    fix: bugs

--- components/dj-trainee/dj-trainee-modal.tsx
+++ components/dj-trainee/dj-trainee-modal.tsx
@@ -13,24 +13,24 @@ export function DJTraineeModal({ isOpen, close, nickname, image, segue, caption,
 		<Modal isOpen={isOpen} close={close}>
 			<div className={styles.panel}>
 				<div className={styles.body}>
-					<div className="flex space-x-4">
-						<figure className="h-48 aspect-square">
+					<div className="flex flex-wrap">
+						<figure className="h-48 aspect-square relative mr-4 mb-4">
 							<Image
 								src={`https://lh3.googleusercontent.com/d/${image}`}
 								alt={`Image of DJ ${nickname}`}
 								className="w-full h-full object-cover"
-								width={200}
-								height={200}
+								fill
+								sizes="(max-width 768px) 100px, 200px"
 							/>
 						</figure>
-						<div>
+						<div className="mb-4">
 							<Dialog.Title as="h1" className={styles.title}>
 								DJ {nickname}
 							</Dialog.Title>
 							<p>{caption}</p>
 						</div>
 					</div>
-					<div className="mt-4 space-y-4 -mx-6 -mb-4 px-4 pb-4 pt-6 bg-[#3f3f3f] text-white">
+					<div className="space-y-4 -mx-6 -mb-4 px-4 pb-4 pt-6 bg-[#3f3f3f] text-white">
 						<VideoItem title="Solo Videoshoot" src={soloVideoShoot} />
 						<VideoItem title="Segue" src={segue} />
 						<VideoItem title="Voiceover Challenge" src={voiceover} />
