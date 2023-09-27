c94ec605a0be9d026591976f4afacd92dc96e140
Jared Sy <jaredblase@gmail.com>
2023-09-27 20:59:37 +0800

    hide: yt embed error

--- app/radio-talents/talent-slider.tsx
+++ app/radio-talents/talent-slider.tsx
@@ -19,19 +19,19 @@ export function TalentSlider({ talents }: TalentSliderProps) {
 				setIndex={setIndex}
 			/>
 			<div className="bg-neutral-900">
-				<div className="grid md:grid-cols-2 place-items-center children:w-full container mx-auto py-8 px-4 gap-8">
+				<div className="grid md:grid-cols-2 place-items-center children:w-full container mx-auto py-20 px-4 gap-8">
 					<div>
 						<h3 className="text-6xl uppercase">DJ {talents[index].nickname}</h3>
 						<h4 className="text-xl font-secondary">{talents[index].name}</h4>
 						<div className="italic text-xl max-w-md mt-8" dangerouslySetInnerHTML={{ __html: talents[index].writeup ?? '' }} />
 					</div>
-					<iframe
+					{/* <iframe
 						className="w-full max-w-lg aspect-video"
 						src={`https://www.youtube.com/embed/W41ZXc3jWBg`}
 						title="YouTube video player"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 						allowFullScreen
-					/>
+					/> */}
 				</div>
 			</div>
 		</>
